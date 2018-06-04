549e919f46360d77c74438e4717a28567e5d6fb3
William Lambert <will.lambert96@gmail.com>
2018-06-04 19:27:37 -0400

    Added a Search Feature and remodeled the action bar

--- app/src/main/java/susa/lambert/william/susa/PostActivity.java
+++ app/src/main/java/susa/lambert/william/susa/PostActivity.java
@@ -1,19 +1,28 @@
 package susa.lambert.william.susa;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Transaction;
+
+import java.util.Date;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -26,7 +35,24 @@ public class PostActivity extends AppCompatActivity {
     private FirebaseFirestore firebaseFirestore;
     private String timeOF = "";
     private String pUser = "";
+    public String loc,desc,addr,uid,img,time, img2, img3,email, availabilty;
+    public int pri, likes_digit;
 
+    public String useremail;
+    public int progressC;
+    Uri post_image;
+    Uri post_image2;
+    Uri post_image3;
+    String timeOf;
+    String Uid;
+    String addy;
+    String des;
+    String tit;
+    String temp_image;
+    String temp_image2;
+    String temp_image3;
+    private String city;
+    private String avail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +90,19 @@ public class PostActivity extends AppCompatActivity {
                 UserPost userPost = documentSnapshot.toObject(UserPost.class);
                 setValues(userPost.title, userPost.desc, userPost.price,
                         userPost.availability, userPost.address, userPost.email);
+
+                        progressC = userPost.price;
+                        addy = userPost.address;
+                        city = userPost.location;
+                        des = userPost.getDesc();
+                        Uid = userPost.userID;
+                        time = userPost.timeOf;
+                        temp_image = userPost.post_image;
+                        temp_image2 = userPost.post_image2;
+                        temp_image3 = userPost.post_image3;
+                        useremail = userPost.email;
+                        avail = userPost.availability;
+                        tit = userPost.title;
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -85,4 +124,76 @@ public class PostActivity extends AppCompatActivity {
         eText.setText(email);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.post_tool, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_home) {
+            Intent intent = new Intent(PostActivity.this, FeedActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_search) {
+            Intent intent = new Intent(PostActivity.this, SearchQActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_store) {
+
+            Date date = new Date();
+
+            String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            UserPost userPost = new UserPost(progressC, addy, city, des, Uid, timeOf, temp_image,
+                    temp_image2, temp_image3, useremail, avail, tit);
+            FirebaseFirestore dt = FirebaseFirestore.getInstance();
+            FirebaseFirestore dl = FirebaseFirestore.getInstance();
+
+            dt.collection("user-likes").document(userid).collection("like").add(userPost);
+
+
+            final DocumentReference sfDocRef = dl.collection("posts").document(time);
+
+            dl.runTransaction(new Transaction.Function<Void>() {
+                @Override
+                public Void apply(Transaction transaction) throws FirebaseFirestoreException {
+                    DocumentSnapshot snapshot = transaction.get(sfDocRef);
+                    double newnum = snapshot.getDouble("likes") + 1;
+                    transaction.update(sfDocRef, "likes", newnum);
+
+                    // Success
+                    return null;
+                }
+            }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    //Log.d(TAG, "Transaction success!");
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            //Log.w(TAG, "Transaction failure.", e);
+                        }
+                    });
+
+
+
+
+
+
+            return true;
+        }
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
