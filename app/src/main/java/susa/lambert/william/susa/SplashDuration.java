86de3bdee08b3c01e4166fccfb005d85924743c7
William Lambert <will.lambert96@gmail.com>
2018-06-13 21:27:35 -0400

    Added splash screen

---
+++ app/src/main/java/susa/lambert/william/susa/SplashDuration.java
@@ -0,0 +1,13 @@
+package susa.lambert.william.susa;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+public class SplashDuration extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SystemClock.sleep(2000);
+    }
+}
